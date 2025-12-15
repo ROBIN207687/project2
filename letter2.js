879936cfa4066570b8f3d9af3fbefeb9ef804fb6
ROBIN207687 <devilmunnda987@gmail.com>
2025-12-15 20:49:45 +0530

    Update letter2.js

--- letter2.js
+++ letter2.js
@@ -1,6 +1,5 @@
 const text =
-  "hey👋 Miss Beautifull ,  I wnna Say Something For a long time. 😉First Time apni feeling share kar rha hu islye hasio mat. Tere baare Kuch likha hai , Ki tu kitni Pyaari🥰, kitni kind😘, or Teri Smile killer hai yaar😂😍, Your laughter, And your every thing is special. Now i feel like Life is a colorless without your presence by my side.  And in my heart Special place that only your friendship. but me tujhe kabhi crush nhi banana chata kyoki crush kabhi nhi milti🥹🥹 , islye me chata hu ki Miss NEHA RAI sirf meri hoke rahee😉🤞 . Jese tu chahti hai ki koi sirf mera ho vese hi I want you Only 🥹, .Or koi mera jesa nhi hoga dusra jo teri itni carega Jo meri tarah tujhe har time secretly notice karega😂😉.. Ek baar trust karke dekh yaar 🥰🤞😊mujhse jyada teri har choti cheej ka khyal koi nhi rakhega shart lagata hu me is baat pe😎😎,... or me bhi chata hu ki tu kabhi mere liye jealous ho jese me feel karta hu tere kisi or ke baare me baat karte hue🤨😠,..or tujhse apne saare secrects share karna chahta hu jo aajtak ksi se nhi kiye kyoki tu sabse jyada special,  Because I like You secretly❤️😘";
-
+  
 const paragraph = text.split("");
 
 let i = 0;
@@ -31,5 +30,6 @@ function startFromBegin() {
   i = 0;
   dashOut(paragraph);
 }
+
 
 startFromBegin();
